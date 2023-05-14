3851eb6633cbc540ee22307d8c576f9bfc5606ca
Vinothdevgit <vinoth51115@gmail.com>
2023-05-14 16:59:10 +0800

    with security and refresh actuator

--- config-client/src/main/java/com/vinothdemo/configclient/RateController.java
+++ config-client/src/main/java/com/vinothdemo/configclient/RateController.java
@@ -1,10 +1,12 @@
 package com.vinothdemo.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 public class RateController {
 
@@ -17,12 +19,17 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
+    @Value("${connstring}")
+    String connstring;
+
+
     @RequestMapping("/rate")
     public String getRate(Model m){
         System.out.println("Inside rate");
         m.addAttribute("rateamount", rate);
         m.addAttribute("lanes", lanecount);
         m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring",connstring);
 
     return "rateview";
     }
---
+++ configserver/src/main/java/com/vinothdemo/configserver/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.vinothdemo.configserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .anyRequest()
+        .permitAll()
+        .and()
+        .csrf()
+        .disable();
+    }
+
+
+}
